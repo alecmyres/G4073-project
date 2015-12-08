ed7f758b56fd48ba490d11f53fa8ed357be24dc3
Alec Myres <alecmyres@Alecs-Air.home>
2015-12-08 03:03:53 -0500

    portfolio work

--- portfolio.py
+++ portfolio.py
@@ -5,7 +5,7 @@
 # Load useful python libraries
 import numpy as np
 import pandas as pd
-
+import datetime, dateutil, os, sys
 
 
 # ---------------------------------------
@@ -35,6 +35,7 @@ class Stock:
         self.curr_amtBought = 0.0
         self.curr_amtSold = 0.0
         self.curr_pnl = 0.0
+        self.last_trade_dt
 
     # size: number of shares (integer)
     # side: "BUY" or "SELL" (string)
@@ -79,13 +80,16 @@ class Portfolio:
         self.start_cash = cash # starting cash
         self.cash_value = cash # current cash value
         self.port_value = 0.0 # sum of abs value of positions 
+        self.fees = 0.0 
 
-    def addTrade(self, ticker, industry, size, side, price):
+    def addTrade(self, ticker, industry, size, side, price, date):
         # Check if stock exists in portfolio, add if needed
         if ticker not in self.stocks.keys():
             self.stocks[ticker] = Stock(ticker, industry)
         # Add trade, update cash value
         self.stocks[ticker].addTrade(size, side, price)
+        if size != 0:
+            self.stocks[ticker].last_trade_dt = date
 
     def updatePortValue(self):
         # Reset portfolio and sector values
@@ -101,29 +105,27 @@ class Portfolio:
                 self.industries[industry] = 0.0
             self.industries[industry] += abs(pv)
             self.cash_value += self.stocks[ticker].pnl
-
+            self.fees += self.stocks[ticker].fees
 
 
 # ---------------------------------------
 # RISK MANAGEMENT
 # ---------------------------------------
 
-# Risk Parameters
-max_stock_percent = 0.025
-max_sector_percent = 0.20
-max_adv_percent = 0.02
-margin_add_percent = 0.50
-margin_call_percent = 0.30 
-stop_loss_percent = 0.10
-
 # Check trade signal for valid portfolio inclusion
 # Set trade size based on risk/portfolio settings
-def checkTrade(ticker, industry, price, action):
+def checkTrade(ticker, industry, price, action, date):
     global portfolio
     global max_stock_percent
     global max_sector_percent
     global max_adv_percent
     global margin_add_percent
+    global slip_rate_bps
+    global days_between_trades
+    # Set action to 0 if last trade too recent
+    last_trade = dateutil.parser.parse(portfolio.stocks[ticker].last_trade_dt)
+    if (dateutil.parser.parse(date) - last_trade).days < days_between_trades:
+        action = 0
     # Set side text based on action value
     if (action == 0) or (action == 1):
         side = "BUY"
@@ -136,11 +138,13 @@ def checkTrade(ticker, industry, price, action):
     if ticker not in portfolio.stocks.keys():
         portfolio.addTrade(ticker, industry, 0, "BUY", 0.0)
     position = portfolio.stocks[ticker].position
-    port_value = portfolio.port_value
+    port_value = portfolio.port_value + portfolio.cash_value
     # Check available portfolio space
     port_alloc = (1.0/margin_add_percent)*portfolio.cash_value - port_value
     av_port_space = max(port_alloc, 0.0)
     # Check available sector space
+    if industry not in portfolio.industries.keys():
+        portfolio.industries[industry] = 0.0
     ind_alloc = portfolio.industries[industry]/port_value
     av_sector_space = max((max_sector_percent - ind_alloc), 0.0)*port_value
     # Check available stock space
@@ -148,22 +152,28 @@ def checkTrade(ticker, industry, price, action):
     av_stock_space = max((max_stock_percent - stock_alloc), 0.0)*port_value
     available_space = min(av_port_space, av_sector_space, av_stock_space)
     # find average daily volume for the stock
-    adv = 1000000 # shares per day
+    #adv = 1000000 # shares per day
     # Set trade size
-    adv_alloc = (adv*price)*max_adv_percent
-    if ((position != 0) & (sign(position) + sign(action) == 0)): # reduce position
-        available_space = adv_alloc
+    #adv_alloc = (adv*price)*max_adv_percent
+    #if ((position != 0) & (np.sign(position) + np.sign(action) == 0)): # reduce position
+    #    available_space = adv_alloc
+    #else:
+    #    available_space = min(adv_alloc, available_space)
+    #trade_size = int(round(available_space/price, -2))
+    trade_size = 100
+    if (position == 100) and (action == 1):
+        trade_size = 0
+    elif (position == -100) and (action == -1):
+        trade_size = 0
     else:
-        available_space = min(adv_alloc, available_space)
-    trade_size = int(round(available_space/price, -2))
+        trade_size = 100
     # Set price to be worse than closing price
-    slip_rate = 5 # in bps
-    adjust = round(max(0.01, price*slip_rate/10000.0), 2)
+    adjust = round(max(0.01, price*slip_rate_bps/10000.0), 2)
     price  = price + (adjust if (side == "BUY") else -adjust)
     # Add trade to portfolio
     if action == 0:
         trade_size = 0
-    portfolio.addTrade(ticker, industry, trade_size, side, price)
+    portfolio.addTrade(ticker, industry, trade_size, side, price, date)
     return
 
 # Check for margin call, sell assets if needed
@@ -174,7 +184,21 @@ def checkMarginCall():
     if excess_margin < 0:
         # Sell assets to get excess margin positive again 
         liq_amt = abs(excess_margin)
-        # TO DO
+        print "MARGIN CALL SELL"
+        # Randomly sell some assets
+        while liq_amt > 0:
+            ticker = np.random.choice(portfolio.stocks.keys())
+            industry = portfolio.stocks[ticker].industry
+            position = portfolio.stocks[ticker].position
+            if position >= 0:
+                side = "SELL"
+                size = position
+            else:
+                side = "BUY"
+                size = abs(position)
+            price = portfolio.stocks['ticker'].last_price
+            portfolio.addTrade(ticker, industry, size, side, price)
+            liq_amt -= size*price
     return
     
 # Check for stop loss
@@ -186,42 +210,126 @@ def checkStopLoss():
         curr_pnl = portfolio.stocks[ticker].curr_pnl
         max_val = portfolio.stocks[ticker].max_mkt_val
         loss = curr_pnl/abs(max_val) if (pos != 0) else 0
-        if loss < stop_loss_percent:
+        if loss < -stop_loss_percent:
             close_dir = "BUY" if (pos < 0) else "SELL"
             industry = portfolio.stocks[ticker].industry
             close_px = portfolio.stocks[ticker].last_price
             portfolio.addTrade(ticker, industry, abs(pos), close_dir, close_px)
+            print "STOP LOSS"
+
+# Load LIBOR rates
+#libor = pd.read_csv('libor.csv')
+#def getLiborRate(year, month):
+#    global libor
+#    lquery = libor.query('year == @year & month == @month')['VALUE']
+#    if len(lquery.index) == 1:
+#        return lquery.item()/100.0
+#    else:
+#        print "ERROR w/ Libor"
+#        return np.nan
+
+
+
+# ---------------------------------------
+# PARAMETERS
+# ---------------------------------------
+
+# Risk Parameters 
+max_stock_percent = 0.025
+max_sector_percent = 1.0 # Set to reasonable level if industry lookup works okay
+max_adv_percent = 0.02
+margin_add_percent = 0.50
+margin_call_percent = 0.30
+stop_loss_percent = 0.10
+slip_rate_bps = 5
+days_between_trades = 5
+
+# Filepath to directory to input files (nothing else)
+# files as specified in email
+files_dir = "/Users/alecmyres/Documents/G4073_Qnt_Mthds/data/"
+
+# Portfolio
+start_cash = 10000000.0
+
+
+
 
 # ---------------------------------------
 # RUN PORTFOLIO, TRADES
 # ---------------------------------------       
 
-# Portfolio start
-start_cash = 1000000.0
+# lookup industry
+def industryLookup(ticker):
+    # do something
+    industry = "technology"
+    return industry
+
+
+# Work through file for a year
+def runYearFile(file):
+    global files_dir
+    global portfolio
+    global dates
+    global pnl
+    global fees
+    global port_value
+    # Load file, fix dates, randomize trade order, sort by date
+    # This will give no preference to any stock in order entry/limits
+    df = pd.read_csv(files_dir + file)
+    df['Date'] = map(lambda x: dateutil.parser.parse(x).strftime('%Y-%m-%d'), df['Date'])
+    df = df.reindex(np.random.permutation(df.index)).sort('Date').reset_index(drop = True)
+    # Work through signals day by day
+    for i in test.index:
+        ticker = test['Stock'][i]
+        date = test['Date'][i]
+        price = test['adjustedPRC'][i]
+        signal = test['Action'][i]
+        industry = industryLookup(ticker)
+        # check trade and update stock pnl
+        checkTrade(ticker, industry, price, signal, date)
+        portfolio.stocks[ticker].updatePNL(price)
+        # resettle calculate/settle portfolio each day
+        if date != last_date:
+            portfolio.updatePortValue()
+            checkStopLoss()
+            checkMarginCall()
+            dates.append(date)
+            cash_value.append(portfolio.cash_value)
+            port_value.append(portfolio.port_value)
+            fees.append(portfolio.fees)
+        # update last date
+        last_date=date
+    # Final portfolio settle
+    portfolio.updatePortValue()
+    checkStopLoss()
+    checkMarginCall()
+    return
+
+
+# Load data, convert date, sort by date
+files = sorted(os.listdir())
+# Initialize portfolio
 portfolio = Portfolio("main", start_cash)
+last_date = '1900-01-01'
+dates = []
+fees = []
+cash_value = []
+port_value = []
 
-# Load LIBOR rates
-libor = pd.read_csv('libor.csv')
-def getLiborRate(year, month):
-    global libor
-    lquery = libor.query('year == @year & month == @month')['VALUE']
-    if len(lquery.index) == 1:
-        return lquery.item()/100.0
-    else:
-        print "ERROR w/ Libor"
-        return np.nan
+# Work through file for each year
+for file in files:
+    runYearFile(file)
 
+# Collect performance measures
+stats = pd.DataFrame()
+stats['Date'] = dates
+stats['cash_value'] = cash_value
+stats['port_value'] = port_value
+stats['fees'] = fees
+stats.to_csv(file_path + "portfolio_stats.csv")
 
-# Day by day portfolio
-# buy/sell
-# pnl updates
-# portfolio updates
-# margin call
-# stop loss
 
 
 # TO DO:
-# Historic fee corrections
-# Interest (short/long/cash)?
-# Handle many same signal stretches
-# remove industry passing in arguments
+# - Industry Lookup
+# - Set file path
